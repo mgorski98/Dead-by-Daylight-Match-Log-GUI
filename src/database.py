8e712d9a3a12583bb30e35e5271b0f7e05ba103f
Maciej <gorski.maciej@o2.pl>
2021-05-05 17:45:35 +0200

    added utility method for downloading images, added downloading survivor and killer portraits

--- src/database.py
+++ src/database.py
@@ -1,9 +1,10 @@
 import sqlalchemy
 import sqlite3
 from bs4 import BeautifulSoup
-from util import saveImageFromURL
+from util import saveImageFromURL, splitGIFIntoImages
 import requests
 from models import Killer, Survivor
+import os
 
 class Database:
     __instance = None
@@ -27,24 +28,49 @@ class Database:
     def update():
         if Database.__instance is None:
             raise ValueError("Database is not initialized. Call Database.init() before invoking any of its methods")
-        BASE_URL = 'https://deadbydaylight.fandom.com/wiki/'
-        KILLERS_URL = f'{BASE_URL}Killers'
-        SURVIVORS_URL = f'{BASE_URL}Survivors'
-        ITEMS_URL = f'{BASE_URL}Items'
-        ADDONS_URL = f'{BASE_URL}Add-ons'
-        OFFERINGS_URL = f'{BASE_URL}Offerings'
+        BASE_URL = 'https://deadbydaylight.fandom.com'
+        BASE_WIKI_URL = 'https://deadbydaylight.fandom.com/wiki/'
+        KILLERS_URL = f'{BASE_WIKI_URL}Killers'
+        SURVIVORS_URL = f'{BASE_WIKI_URL}Survivors'
+        ITEMS_URL = f'{BASE_WIKI_URL}Items'
+        ADDONS_URL = f'{BASE_WIKI_URL}Add-ons'
+        OFFERINGS_URL = f'{BASE_WIKI_URL}Offerings'
 
         killersDoc = requests.get(KILLERS_URL).content
         killersParser = BeautifulSoup(killersDoc, 'html.parser')
         mainDiv = killersParser.find('div', attrs={'style': 'color: #fff;'})
         aTags = mainDiv.find_all('a')
         killers = [Killer(killerID=i, killerName=aTags[j].get('title', ''), killerAlias=aTags[j+1].get('title', '')) for i, j in enumerate(range(0, len(aTags), 2))]
+        killerUrls = [f"{BASE_URL}{a.get('href', '')}" for a in aTags[::2]]
+        #todo: download killer portrait for each of the killers
+        for i, url in enumerate(killerUrls):
+            killerPageParser = BeautifulSoup(requests.get(url).content, 'html.parser')
+            infoTable = killerPageParser.find('table', attrs={"class": "infoboxtable"})
+            imgTag = infoTable.find('img')
+            imgUrl = imgTag.get('src', '')
+            name = killers[i].killerAlias.lower().replace(' ', '-')
+            dest = f'../images/killers/the-{name}.png'
+            if not os.path.exists(dest):
+                saveImageFromURL(imgUrl, dest)
+
 
         survivorsDoc = requests.get(SURVIVORS_URL).content
         survivorsParser = BeautifulSoup(survivorsDoc, 'html.parser')
         mainDiv = survivorsParser.find('div', attrs={'style': 'color: #fff;'})
         aTags = mainDiv.find_all('a')
         survivors = [Survivor(survivorID=i, survivorName=a.get('title', '')) for i,a in enumerate(aTags[::2])]
+        survivorUrls = [f"{BASE_URL}{a.get('href', '')}" for a in aTags[::2]]
+
+        for i, url in enumerate(survivorUrls):
+            survivorPageParser = BeautifulSoup(requests.get(url).content, 'html.parser')
+            infoTable = survivorPageParser.find('table', attrs={"class": "infoboxtable"})
+            imgTag = infoTable.find('img')
+            imgUrl = imgTag.get('src', '')
+            name = survivors[i].survivorName.lower().replace(' ', '-')
+            dest = f'../images/survivors/{name}.png'
+            if not os.path.exists(dest):
+                saveImageFromURL(imgUrl, dest)
+            #todo: download gifs here and split them into perk icons
 
         print(killers)
         print(survivors)
--- src/util.py
+++ src/util.py
@@ -1,5 +1,10 @@
-
+import requests
 
 
 def saveImageFromURL(url: str, dest: str):
+    request = requests.get(url,stream=True)
+    with open(dest,mode='wb') as f:
+        f.write(request.content)
+
+def splitGIFIntoImages(gifPath: str, frameMarking='I'): #in format e.g. frame-I, frame-II, should also work with integers if 1 is passed
     pass
