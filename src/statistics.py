eb7e3c8ad82f8d5dde56373134e5dfa0dac964df
Maciej <gorski.maciej@o2.pl>
2021-06-06 16:27:59 +0200

    added StatisticsExporter class and finished statistics classes hierarchy (can be a subject to change in the future if i think about new ways of calculating statistics)

--- src/statistics.py
+++ src/statistics.py
@@ -1,36 +1,51 @@
 from dataclasses import dataclass
+from abc import ABC
 
 import pandas as pd
 
-from models import DBDMatch, SurvivorMatch, KillerMatch, Survivor, Killer, Realm, GameMap
+from models import DBDMatch, SurvivorMatch, KillerMatch, Survivor, Killer, Realm, GameMap, ItemType, SurvivorMatchResult
 
 
-@dataclass
-class GeneralMatchStatistics:
+#todo: add export method so you can export statistics to external file (json, xml, some other formats like yaml)
+
+@dataclass(frozen=True)
+class EliminationInfo(object):
+    sacrifices: int
+    kills: int
+    disconnects: int
+
+class MatchStatistics(ABC):
+    pass
+
+@dataclass(frozen=True)
+class GeneralMatchStatistics(MatchStatistics):
     totalPoints: int
     mostCommonMap: GameMap
     mostCommonMapRealm: Realm
 
-@dataclass
-class GeneralKillerMatchStatistics(object):
+@dataclass(frozen=True)
+class GeneralKillerMatchStatistics(MatchStatistics):
     totalSacrifices: int
     totalKills: int
     totalDisconnects: int
+    gamesPlayedWithKiller: dict[Killer, int]
 
-@dataclass
-class GeneralSurvivorMatchStatistics(object):
+@dataclass(frozen=True)
+class GeneralSurvivorMatchStatistics(MatchStatistics):
+    gamesPlayedWithSurvivor: dict[Survivor, int]
+    matchResultsHistogram: dict[SurvivorMatchResult, int]
+    mostCommonItemType: ItemType
+
+@dataclass(frozen=True)
+class TargetStatistics(MatchStatistics):
     pass
 
-@dataclass
-class TargetStatistics(object):
+@dataclass(frozen=True)
+class TargetKillerStatistics(MatchStatistics):
     pass
 
-@dataclass
-class TargetKillerStatistics(object):
-    pass
-
-@dataclass
-class TargetSurvivorStatistics(object):
+@dataclass(frozen=True)
+class TargetSurvivorStatistics(MatchStatistics):
     pass
 
 class StatisticsCalculator(object):
@@ -56,3 +71,21 @@ class StatisticsCalculator(object):
 
     def calculateGeneral(self) -> GeneralMatchStatistics:
         raise NotImplementedError()
+
+
+class StatisticsExporter(object):
+
+    __export_types__ = {
+        "xml": None,
+        "json": None,
+        "yaml": None
+    }
+
+    def __init__(self, exportType: str):
+        self.exportHandler = StatisticsExporter.__export_types__.get(exportType)
+        if self.exportHandler is None:
+            validOptions = ', '.join(StatisticsExporter.__export_types__.keys())
+            raise ValueError(f"Unknown export type: {exportType}. Valid options are: {validOptions}")
+
+    def export(self, statistics: MatchStatistics, destinationPath: str) -> None:
+        pass
