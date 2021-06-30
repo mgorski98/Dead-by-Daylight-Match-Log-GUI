686e9c15d1c081c4a890a35407cefed2392bd76a
Maciej <gorski.maciej@o2.pl>
2021-06-30 17:23:18 +0200

    updated windows build script

--- build_scripts/build-win.bat
+++ build_scripts/build-win.bat
@@ -1,2 +1,3 @@
-python ../src/setup.py build -b ../builds
+del /S /Q ..\builds\* rem /S deletes all files from subdirs, /Q is a "quiet mode" - doesnt ask you if you are sure you want to delete the files
+python ../src/setup.py build -b ../builds rem build process
 pause
